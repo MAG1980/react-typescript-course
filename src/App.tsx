d62254906453e53b8db43b8f4c7b00f82daa9d7b
MAG1980 <al-1980@inbox.ru>
2024-03-10 18:14:59 +0300

    Типизация useState.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import { TodoItem } from "./components/TodoItem";
+import { useState } from "react";
 
+type Todo={
+  id:number
+  title:string
+  completed:boolean
+
+}
 function App() {
-
+  const [text, setText] = useState('');
+  const [todos, setTodos] = useState<Todo|null>(null);
 
   return (
     <>
       <h1>Vite + React</h1>
+      <p>{text}</p>
+      <div>{todos?.title}</div>
       <TodoItem
         id={111}
         title={"First todo"}
